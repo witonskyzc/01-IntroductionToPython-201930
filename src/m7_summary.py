25280b8d7e62cbf7819900e94654b63021e1a41a
witonsz <witonskyzc@gmail.com>
2019-03-04 11:48:09 -0500

    Day 1 || m7

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,36 +2,49 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Zach Witonsky.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Write code that accomplishes the following (and ONLY the following),
 #   in the order listed:
 #
 #    - Constructs a SimpleTurtle with a  'blue'  Pen.
-#
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+turtle = rg.SimpleTurtle()
+turtle.pen = rg.Pen('blue', 3)
+
 #    - Makes the SimpleTurtle go straight UP 200 pixels.
-#
+turtle.left(90)
+turtle.forward(200)
+
 #    - Makes the SimpleTurtle lift its pen UP
 #         (so that the next movements do NOT leave a "trail")
 #         HINT: Use the "dot trick" to figure out how to do this.
-#
+turtle.pen_up()
+
 #    - Makes the SimpleTurtle go to the Point at (100, -40).
-#
+turtle.go_to(rg.Point(100, -40))
+
 #    - Makes the SimpleTurtle put its pen DOWN
 #         (so that the next movements will return to leaving a "trail").
-#
+turtle.pen_down()
+
 #    - Makes the SimpleTurtle's pen have color 'green' and thickness 10.
-#
+turtle.pen = rg.Pen('green', 10)
+
 #    - Makes the SimpleTurtle go 150 pixels straight DOWN.
-#
+turtle.backward(150)
+
 #   Don't forget to:
 #     - import rosegraphics and construct a TurtleWindow
 #          at the BEGINNING of your code, and to
@@ -43,3 +56,4 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+window.close_on_mouse_click()
